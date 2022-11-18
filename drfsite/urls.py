f1e9c957913c1c01cf6ab445ca81c85fef4d22d1
Malets_Sergey <maletssergey@gmail.com>
2022-11-18 12:23:27 +0300

    8. Routers (SimpleRouter)

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from women.views import *
 from rest_framework import routers
 
@@ -22,8 +22,11 @@ from rest_framework import routers
 router = routers.SimpleRouter()
 router.register(r'women', WomenViewSet)
 
+print(router.urls)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/womenlist/', WomenViewSet.as_view({'get':'list'})),
-    path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put':'update'})),
+    path('api/v1/', include(router.urls)),  #http://127.0.0.1:8000/api/v1/women/
+    # path('api/v1/womenlist/', WomenViewSet.as_view({'get':'list'})),
+    # path('api/v1/womenlist/<int:pk>/', WomenViewSet.as_view({'put':'update'})),
 ]
