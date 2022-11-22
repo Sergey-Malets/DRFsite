4e595a70200e296c4335a44d03aacfec3c589f11
Malets_Sergey <maletssergey@gmail.com>
2022-11-22 15:38:11 +0300

    14. Pagination

--- women/views.py
+++ women/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.views import APIView
 from .models import Women, Category
@@ -8,10 +9,16 @@ from .serializers import WomenSerializer
 
 
 
+class WomenAPIListPagination(PageNumberPagination):
+    page_size = 3  #дефолтное кол-во записей на странице
+    page_size_query_param = 'page_size'  #возможность пользователю чрез get запрос выбрать сколько отобразится на странице
+    max_page_size = 10000  #это максимально допустимое значение page_size
+
 class WomenAPIList(generics.ListCreateAPIView):
     queryset = Women.objects.all()
     serializer_class = WomenSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = WomenAPIListPagination
 
 class WomenAPIUpdate (generics.RetrieveUpdateAPIView):
     queryset = Women.objects.all() # мы как будто выбираем все записи, но UpdateAPIView выбирет только одну конкретную
